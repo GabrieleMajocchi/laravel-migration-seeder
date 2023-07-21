f5c3042324728e5778c594d75f9fca36b0350f9e
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-21 16:41:52 +0200

    printed on screen the trains info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
-@section('title', 'Welcome')
+@section('title', 'Welcome to the train station')
 
 @section('main-section')
-    <h1>
-        Welcome, this is Main section area
-    </h1>
+    <h2 class="text-center">Trains on the binary:</h2>
+    <div class="movies text-center d-flex justify-content-between flex-wrap">
+
+    @foreach ($trains as $train)
+        <div class="card" style="width: calc(100% / 3 - 15rem); margin: 2rem 7.5rem;">
+            <div class="card-body">
+                <h5 class="card-title">Company: {{$train->company}}</h5>
+                <p class="card-text">Start station:{{$train->start_station}}</p>
+                <p class="card-text">Arrive station:{{$train->end_station}}</p>
+                <p class="card-text">Departure time:{{$train->departure_time}}</p>
+                <p class="card-text">Arrival time:{{$train->arrival}}</p>
+                <p class="card-text">Train code:{{$train->train_code}}</p>
+                <p class="card-text">Train sections:{{$train->train_section}}</p>
+                <p class="card-text">On time:{{$train->on_time}}</p>
+                <p class="card-text">Deleted:{{$train->deleted}}</p>
+            </div>
+        </div>
+    @endforeach
+    </div>
 @endsection
