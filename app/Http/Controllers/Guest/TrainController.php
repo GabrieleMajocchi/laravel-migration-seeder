05bec49e586dc86d3114b795772a4b9874c660d1
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-21 16:41:21 +0200

    added  get_trains function to get trains info from db

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Train;
+
 class TrainController extends Controller
 {
-    //
+    public function get_trains(){
+
+        $trains = Train::all();
+
+        return view('welcome', compact('trains'));
+    }
 }
