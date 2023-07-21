2146a2cbf76002671cee19efba1e402af7b61b17
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-21 16:41:42 +0200

    added the route with function to get the data from db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Guest\TrainController as TrainController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [TrainController::class, 'get_trains'])->name('homepage');
